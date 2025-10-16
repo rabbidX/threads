633fefa639cdf6b48c57e4b456acb12d9e73ab42
Dmitry Garyanov <dmi3jX@ya.ru>
2025-10-17 00:58:24 +0300

    Optimize adding

--- src/main/java/me/garyanov/threads/racecondition/MultithreadedQueueAdder.java
+++ src/main/java/me/garyanov/threads/racecondition/MultithreadedQueueAdder.java
@@ -3,19 +3,12 @@ package main.java.me.garyanov.threads.racecondition;
 import main.java.me.garyanov.threads.racecondition.exception.NoMoreElementsException;
 import main.java.me.garyanov.threads.racecondition.queue.NaturalNumbersQueue;
 
-import java.util.UUID;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class MultithreadedQueueAdder {
     private final static int THREAD_NUMBER = 10;
     private final NaturalNumbersQueue queue;
     private final int[] sums = new int[THREAD_NUMBER];
-    String[] threadNames = Stream.generate(UUID::randomUUID)
-            .limit(THREAD_NUMBER)
-            .map(UUID::toString)
-            .toArray(String[]::new);
-
 
     public MultithreadedQueueAdder(NaturalNumbersQueue queue) {
         this.queue = queue;
@@ -24,7 +17,8 @@ public class MultithreadedQueueAdder {
     public int getSum() {
         Thread[] threads = new Thread[THREAD_NUMBER];
         for (int i = 0; i < THREAD_NUMBER; i++) {
-            threads[i] = new Thread(this::setThreadsSum, threadNames[i]);
+            final int threadIndex = i;
+            threads[i] = new Thread(() -> setThreadsSum(threadIndex));
         }
         for (Thread thread: threads) {
             thread.start();
@@ -39,23 +33,14 @@ public class MultithreadedQueueAdder {
         return IntStream.of(sums).sum();
     }
 
-    private void setThreadsSum() {
+    private void setThreadsSum(int i) {
         while (true) {
             try {
-                sums[getThreadNumber()] += queue.getNext();
+                sums[i] += queue.getNext();
             } catch (NoMoreElementsException e) {
                 break;
             }
         }
     }
 
-    private int getThreadNumber() {
-        var threadName = Thread.currentThread().getName();
-        for (int i = 0; i < THREAD_NUMBER; i++) {
-            if (threadName.equals(threadNames[i])) {
-                return i;
-            }
-        }
-        throw new RuntimeException("Cannot define thread number");
-    }
 }
